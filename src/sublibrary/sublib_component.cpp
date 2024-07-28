9298082844e7feef6be37161e13bf9f51cac20ae
Helmut Waldschmidt <helmut.waldschmidt@gmail.com>
2024-07-28 17:20:45 +0200

    add spdlog

--- src/sublibrary/sublib_component.cpp
+++ src/sublibrary/sublib_component.cpp
@@ -1,6 +1,7 @@
 #include "sublib_component.h"
 
 #include <fmt/core.h>
+#include <spdlog/spdlog.h>
 
 namespace name::sublibrary {
 void SubLibComponent::print(int a) {
@@ -14,6 +15,7 @@ void SubLibComponent::print(int a) {
 }
 
 int SubLibComponent::add(int a, int b) {
+    spdlog::info("Adding numbers: ");
     const int magic_number = 10;
     if (a > magic_number) {
         fmt::print("a is greater than 10\n");
